f531c34d282c407487ef7fdcb786ed84990122f1
DESKTOP-8OMQLF3\27798 <277983923@qq.com>
2021-09-08 18:34:48 +0800

    add 4 5

--- cmakeproject/cmakeproject.cpp
+++ cmakeproject/cmakeproject.cpp
@@ -11,5 +11,7 @@ int main()
 	std::cout << "1" << std::endl;
 	std::cout << "2" << std::endl;
 	std::cout << "3" << std::endl;
+	std::cout << "4 add" << std::endl;
+	std::cout << "5 temp" << std::endl;
 	return 0;
 }
